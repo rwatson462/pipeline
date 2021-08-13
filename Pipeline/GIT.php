058b2fbb44b3c4e1067e6b007d35eaa6b209148a
Rob <rob.watson@me.com>
2021-08-13 11:37:12 +0100

    use GIT class when issuing commands

--- Pipeline/GIT.php
+++ Pipeline/GIT.php
@@ -2,11 +2,52 @@
 
 namespace Pipeline;
 
-// This class provides all GIT-related functionality
+/**
+ * The purpose of this GIT class is to wrap the GIT shell commands by
+ * returning bool(true) when commands are successful and throwing exceptions
+ * when they are not.
+ * This allows easier control during PHP script execution later.
+ */
 class GIT
 {
-   public function cmd( $cmd )
+   /**
+    * Executes a GIT command
+    * @param string $cmd the command to run
+    * @param array $output populated with both stdout and stderr output
+    * @return true on success
+    * @throws \Exception on non-zero status code of command
+    */
+   private function exec( string $cmd, ?array &$output = null ): bool
    {
-      return `git $cmd`;
+      $result = 0;
+      // $cmd should be the basic git command like 'git checkout develop'
+      // we'll redirect stderr to stdout so we can capture the output on failure
+      $cmd .= ' 2>&1';
+      exec( $cmd, $output, $result );
+
+      // git commands return a non-zero status on failure
+      if( $result === 0 ) return true;
+
+      throw new \Exception( implode( "\n", $output ) );
+   }
+
+   public function checkout( string $branch, string $new_branch_name = '') : bool
+   {
+      if( $new_branch_name )
+      {
+         return $this->exec( "git checkout -b $new_branch_name $branch" );
+      }
+
+      return $this->exec( "git checkout $branch" );
+   }
+
+   public function add( string $filename ): bool
+   {
+      return $this->exec( "git add $filename" );
+   }
+
+   public function commit( string $message ): bool
+   {
+      return $this->exec( "git commit -m $message" );
    }
 }
